9c58cf109fd0f330eb371a2cc53b6599f7479579
Zeddli <ruj8320@gmail.com>
2025-07-18 10:55:30 +0800

    Stats endpoint

--- src/controllers/StatisticsController.js
+++ src/controllers/StatisticsController.js
@@ -3,14 +3,25 @@
  */
 const service = require('../services/StatisticsService')
 
+function sendError(res, err) {
+  const status = err.status || err.httpStatus || 500;
+  const body = { message: err.message };
+  if (err.details) body.details = err.details;
+  res.status(status).json(body);
+}
+
 /**
  * Get distribution statistics
  * @param {Object} req the request
  * @param {Object} res the response
  */
 async function getDistribution (req, res) {
+  try {
   const result = await service.getDistribution(req.query)
   res.send(result)
+  } catch (err) {
+    sendError(res, err)
+  }
 }
 
 /**
@@ -19,8 +30,12 @@ async function getDistribution (req, res) {
  * @param {Object} res the response
  */
 async function getHistoryStats (req, res) {
+  try {
   const result = await service.getHistoryStats(req.authUser, req.params.handle, req.query)
   res.send(result)
+  } catch (err) {
+    sendError(res, err)
+  }
 }
 
 /**
@@ -29,8 +44,12 @@ async function getHistoryStats (req, res) {
  * @param {Object} res the response
  */
 async function getMemberStats (req, res) {
+  try {
   const result = await service.getMemberStats(req.authUser, req.params.handle, req.query, true)
   res.send(result)
+  } catch (err) {
+    sendError(res, err)
+  }
 }
 
 /**
@@ -39,8 +58,12 @@ async function getMemberStats (req, res) {
  * @param {Object} res the response
  */
 async function getMemberSkills (req, res) {
+  try {
   const result = await service.getMemberSkills(req.params.handle)
   res.send(result)
+  } catch (err) {
+    sendError(res, err)
+  }
 }
 
 /**
@@ -49,8 +72,12 @@ async function getMemberSkills (req, res) {
  * @param {Object} res the response
  */
 async function createMemberSkills (req, res) {
+  try {
   const result = await service.createMemberSkills(req.authUser, req.params.handle, req.body)
   res.send(result)
+  } catch (err) {
+    sendError(res, err)
+  }
 }
 
 /**
@@ -59,14 +86,78 @@ async function createMemberSkills (req, res) {
  * @param {Object} res the response
  */
 async function partiallyUpdateMemberSkills (req, res) {
+  try {
   const result = await service.partiallyUpdateMemberSkills(req.authUser, req.params.handle, req.body)
   res.send(result)
+  } catch (err) {
+    sendError(res, err)
+  }
+}
+
+/**
+ * Create member history statistics
+ * @param {Object} req the request
+ * @param {Object} res the response
+ */
+async function createHistoryStats (req, res) {
+  try {
+    const result = await service.createHistoryStats(req.authUser, req.params.handle, req.body)
+    res.status(201).send(result)
+  } catch (err) {
+    sendError(res, err)
+  }
+}
+
+/**
+ * Update member history statistics
+ * @param {Object} req the request
+ * @param {Object} res the response
+ */
+async function updateHistoryStats (req, res) {
+  try {
+    const result = await service.updateHistoryStats(req.authUser, req.params.handle, req.body)
+    res.status(200).send(result)
+  } catch (err) {
+    sendError(res, err)
+  }
+}
+
+/**
+ * Create member statistics
+ * @param {Object} req the request
+ * @param {Object} res the response
+ */
+async function createMemberStats (req, res) {
+  try {
+    const result = await service.createMemberStats(req.authUser, req.params.handle, req.body)
+    res.status(201).send(result)
+  } catch (err) {
+    sendError(res, err)
+  }
+}
+
+/**
+ * Update member statistics
+ * @param {Object} req the request
+ * @param {Object} res the response
+ */
+async function updateMemberStats (req, res) {
+  try {
+    const result = await service.updateMemberStats(req.authUser, req.params.handle, req.body)
+    res.status(200).send(result)
+  } catch (err) {
+    sendError(res, err)
+  }
 }
 
 module.exports = {
   getDistribution,
   getHistoryStats,
+  createHistoryStats,
+  updateHistoryStats,
   getMemberStats,
+  createMemberStats,
+  updateMemberStats,
   getMemberSkills,
   createMemberSkills,
   partiallyUpdateMemberSkills
